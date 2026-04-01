8422b620297e2da33b7ad64a5ce8c45c34ec600c
sgcasas <sebastian.casas.m@utec.edu.pe>
2026-04-01 15:21:14 -0500

    3.1

--- main.cpp
+++ main.cpp
@@ -14,11 +14,17 @@ public:
     if (shape.empty() || tam > 3 ) throw std::invalid_argument("El tensor debe tener máximo 3 dimensiones");
     for (int i = 0; i < tam; i++) { tot *= shape[i]; }
     if (tot != values.size()) throw std::invalid_argument("La cantidad de valores no coincide con el tamaño del tensor");
-    static Tensor zeros(const vector<size_t>& s) {
-
-    }
   }
+  void set_values(vector<double>& v) { values = v; }
+  void set_shape(vector<size_t>& s) { shape = s; }
+  static Tensor zeros(const vector<size_t>& s);
 };
+
+Tensor Tensor::zeros(const vector<size_t>& s) {
+  int tot = 1;
+  for (int i = 0; i < s.size(); i++) { tot *= s[i]; }
+  return Tensor(s, vector<double>(tot, 0));
+}
 
 int main() {
   return 0;
